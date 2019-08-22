e75cdbf8f22a02525e86a392c5e696b4b27f3579
Michal Terranova <michalterranova@mail.com>
2019-08-21 20:24:33 -0400

    notes

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 //require .env for passcodes
 require ('dotenv').config();
 
+where to import the keys and pass them to liri
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
